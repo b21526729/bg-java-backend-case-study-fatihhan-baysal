e70ec5f5a7d44f2ce6b036fabfc2c18300074535
fatihhanbaysal <fatihhanbaysal@hotmail.com>
2025-08-11 22:11:39 +0300

    initial commit

---
+++ BilisimGarajiTask/src/main/java/com/example/bilisimgarajitask/BilisimGarajiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilisimgarajitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilisimGarajiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilisimGarajiTaskApplication.class, args);
+    }
+
+}
---
+++ BilisimGarajiTask/src/test/java/com/example/bilisimgarajitask/BilisimGarajiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilisimgarajitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilisimGarajiTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
