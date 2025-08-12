de922e1350148d95b03d471c555957344d938915
fatihhanbaysal <fatihhanbaysal@hotmail.com>
2025-08-12 19:35:10 +0300

    fix dependencies, disable Spring Security Login

---
+++ BilisimGarajiTask/src/main/java/com/example/bilisimgarajitask/common/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.bilisimgarajitask.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(a -> a
+                .requestMatchers("/swagger-ui.html","/swagger-ui/**","/v3/api-docs/**","/api-docs/**").permitAll()
+                .anyRequest().permitAll()
+        )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
